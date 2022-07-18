c4f1e9f21a8b546c363ff51447cd426d43e1445a
manish <Manishbisht9711@gmail.com>
2022-07-18 13:23:22 +0530

    products page

--- pages/Iraninaturals.js
+++ pages/Iraninaturals.js
@@ -3,50 +3,77 @@ import Navbar from "../components/Navbar";
 import { storefront } from "../utils/index.js";
 import Link from "next/link";
 import { useState, useEffect } from "react";
+import styles from "../styles/ProductsPage.module.css";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+import { Rating } from "@mui/material";
 
 const Iraninaturals = ({ products }) => {
+  // console.log(products.edges);
+
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
     setMounted(true);
   }, []);
 
+  const options = {
+    size: "small",
+    value: 4,
+    readOnly: true,
+    precision: 0.5,
+  };
+
   return (
     mounted && (
       <>
         <Navbar />
-        <section className="text-gray-600 body-font">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="flex flex-wrap -m-4">
-              {products.edges.map((item) => {
-                const product = item.node;
-                const id = product.id;
-                const title = product.title;
-                const handle = product.handle;
-                const image = product.images.edges[0].node;
-                const price = product.priceRange.minVariantPrice.amount;
-                return (
-                  <Link key={id} href={`/product/${handle}`}>
-                    <a className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                      <a className="block relative h-48 rounded overflow-hidden">
-                        <img
-                          alt={image.altText}
-                          className="object-cover object-center w-full h-full block"
-                          src={image.url}
-                        />
-                      </a>
-                      <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                          {title}
-                        </h3>
-                        <p className="mt-1">{price}</p>
+        <div className={styles.productcontainer}>
+          {products.edges.map((item) => {
+            const product = item.node;
+            const id = product.id;
+            const title = product.title;
+            const handle = product.handle;
+            const image = product.images.edges[0].node;
+            const price = product.priceRange.minVariantPrice.amount;
+
+            const titlemore = title;
+
+            if (title.length > 50) {
+              titlemore = title.substring(0, 50) + "...";
+            }
+
+            return (
+              <Link key={id} href={`/product/${handle}`}>
+                <a>
+                  <div className={styles.card}>
+                    <div className={styles.image}>
+                      <img
+                        src={image.url}
+                        alt={image.altText}
+                        className={styles.product_image}
+                      />
+                    </div>
+                    <div className={styles.product_info}>
+                      <div className={styles.name}>{titlemore}</div>
+                      <div className={styles.ratings}>
+                        <Rating {...options} />
+                        <span className={styles.reviews}>23 reviews</span>
                       </div>
-                    </a>
-                  </Link>
-                );
-              })}
-            </div>
-          </div>
-        </section>
+                      <div className={styles.addtocart}>
+                        <div className={styles.price}>
+                          ₹{price} <span>₹130</span>
+                        </div>
+                        <div className={styles.cart_button}>
+                          add to cart <AiOutlineShoppingCart />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
       </>
     )
   );
